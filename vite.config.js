e93934494830071af2a002e4e8f69d11bbda1a56
4Sighteducation <admin@vespa.academy>
2025-08-05 19:50:20 +0100

    Cachebuster3c

--- vite.config.js
+++ vite.config.js
@@ -25,12 +25,12 @@ export default defineConfig({
       external: [],
       output: {
         // JS output
-                  entryFileNames: 'vuedash3b.js',
+                  entryFileNames: 'vuedash3c.js',
         
         // CSS output - extracted to separate file
         assetFileNames: (assetInfo) => {
           if (assetInfo.name?.endsWith('.css')) {
-            return 'vuedash3b.css'
+            return 'vuedash3c.css'
           }
           return '[name][extname]'
         },
